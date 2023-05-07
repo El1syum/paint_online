7e12102edcdb7a4a18c11e7889bd39f19b91b088
el1syum <kilmol606@gmail.com>
2023-05-07 23:26:38 +0300

    initial

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/canvas.scss'
+
+const Canvas = () => {
+    return (
+        <div className='canvas'>
+            <canvas width={600} height={400}/>
+        </div>
+    );
+};
+
+export default Canvas;
